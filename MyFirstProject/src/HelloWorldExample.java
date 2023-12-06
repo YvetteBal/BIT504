63fc3aaee289a0a5d2f336eeb7d30f83f4b718cd
YvetteBal <Yvett@192.168.1.71>
2023-12-07 09:48:30 +1300

    pushpush

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -1,10 +1,15 @@
-
 public class HelloWorldExample {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, World!");
-
-	}
-
-}
+ 
+ 
+ 
+     public static void main(String[] args) {
+ 
+         // Output Hello World to the user
+ 
+         System.out.println("Hello World");
+ 
+     }
+ 
+ 
+ 
+ }
